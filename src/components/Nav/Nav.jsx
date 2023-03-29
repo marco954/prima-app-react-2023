d6d4fedfe2476e30aa0713e9fe05015a07ee13f6
Marco Veronelli <mveronelli954@gmail.com>
2023-03-29 09:35:19 +0200

    aggiunta menu

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,23 +2,22 @@ import "./Nav.css"
 
 
 function Nav() {
-    return(
-        <nav className="flexbetween">
-            <span> MyPage</span>
-        <ul>
-            <li>
-                <a href="##">home</a>
-            </li>
-            <li>
-                <a href="##">why</a>
-            </li>
-            <li>
-                <a href="##">how</a>
-            </li>       
-        </ul>      
+  return (
+    <nav className="flexbetween">
+      <span> MyPage</span>
+      <img id="burgher" src="./assets/menu.svg" />
+      <ul>
+        <li>
+          <a href="##">home</a>
+        </li>
+        <li>
+          <a href="##">why</a>
+        </li>
+        <li>
+          <a href="##">how</a>
+        </li>
+      </ul>
     </nav>
-        
-    );
-    
+  );
 }
 export default Nav
